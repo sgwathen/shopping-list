a55dff7bf319451917516b89ff4630db50ae01d7
sgwathen <samuelwathen@MAC.local>
2014-09-21 22:47:13 -0500

    jquery

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
+	$(document).keypress(function(e) {
+		if(e.keycode == 13) {
+			e.preventDefault();
+			$('#add').click();
+		}
+	});
 	
+	$(document).on('click', '#addItem', function() {
+		var item =$('#add').val('');
+		var list = $('#list');
+		$.each(item, function() {
+			var div = $('<div/>')
+				.addClass('unchecked')
+				.appendTo(list);	
+		});
+	});
+	
+	$(document).on('click', '.unchecked', function () {
+		$(this).closest('div').toggleClass('checked');
+	});
+	
+	$(document).on('click', '.remove', function() {
+		$(this).parent().closest('div').remove();	
+	});
 });// JavaScript Document
