209a564585301a0767bcdbc19dbdb9aba4455564
sgwathen <samuelwathen@MAC.local>
2014-09-22 18:56:30 -0500

    Issues

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 $(document).ready(function() {
-	$(document).keypress(function(e) {
-		if(e.keycode == 13) {
-			e.preventDefault();
-			$('#add').click();
+	$('#addItem').on('click', '#submit', function() {
+		var addItem = $('#add').val();
+		if (addItem == "" || addItem.trim().length == 0) {
+			alert("Enter an item");
+		} else {
+			$('#items ul').append('<li>' + addItem + '</li><div class="remove"></div>');
+			$('#add').val('');
 		}
 	});
 	
-	$(document).on('click', '#addItem', function() {
-		var item =$('#add').val('');
-		var list = $('#list');
-		$.each(item, function() {
-			var div = $('<div/>')
-				.addClass('unchecked')
-				.appendTo(list);	
-		});
-	});
-	
-	$(document).on('click', '.unchecked', function () {
-		$(this).closest('div').toggleClass('checked');
+	//Enter to trigger click
+	$(document).keypress(function(e) {
+		if(e.keycode == 13) {
+			e.preventDefault();
+			enter.click();
+		}
 	});
 	
 	$(document).on('click', '.remove', function() {
-		$(this).parent().closest('div').remove();	
+		$(this).closest('li').remove();
 	});
+	
 });// JavaScript Document
