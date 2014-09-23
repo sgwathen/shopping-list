550f31d1b20bccd28c2083f011c6bcf8375fd049
sgwathen <samuelwathen@MAC.local>
2014-09-23 18:48:25 -0500

    Final_trial

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 $(document).ready(function() {
 	$('#addItem').on('click', '#submit', function() {
-		var addItem = $('#add').val();
-		if (addItem == "" || addItem.trim().length == 0) {
+		event.preventDefault();
+		var item = $('#add').val();
+		if (item == "" || item.trim().length == 0) {
 			alert("Enter an item");
 		} else {
-			$('#items ul').append('<li>' + addItem + '</li><div class="remove"></div>');
+			$('ul').append('<li>' + item + '</li><div class="remove"></div>');
 			$('#add').val('');
 		}
+	})
 	});
 	
-	//Enter to trigger click
-	$(document).keypress(function(e) {
-		if(e.keycode == 13) {
-			e.preventDefault();
-			enter.click();
-		}
+$(document).keypress(function(e) {
+	if(e.keycode == 13) {
+		e.preventDefault();
+		enter.click();
+	}
 	});
-	
-	$(document).on('click', '.remove', function() {
-		$(this).closest('li').remove();
+
+$(document).on('click', 'li', function() {
+	$(this).toggleClass('checked');
+	})
+	.on('click', '.remove', function() {
+		$(this).prev('li').remove();
+		$(this).closest('.remove').remove();
 	});
-	
-});// JavaScript Document
+
